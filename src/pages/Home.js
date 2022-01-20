4712b48d092494571410cfa4f581606b40387c62
Ryan <ryan@RyandeMacBook-Pro.local>
2022-01-20 16:44:28 +0800

    login link add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Home = () => {
     return (
         <div>
             This is Home ...
+            <Link to='/login' className="subList">登入</Link>
         </div>
     )
 }
